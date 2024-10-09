31befce85e17c3b170f70762681e2d05ea86f7ae
G00419511@atu.ie <G00419511@atu.e>
2024-10-09 14:51:39 +0100

    Created more statements

---
+++ src/main/java/ie/atu/week4cicd/Product.java
@@ -0,0 +1,18 @@
+package ie.atu.week4cicd;
+
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @NotBlank(message="You must enter a name")
+    private String name;
+    @Min(value=1, message="Must be greater or equal to 1")
+    private int id;
+}
